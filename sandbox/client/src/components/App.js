498781e6a790382e07d4c2b5044ab5413802158f
Patrick <pjmorgan716@gmail.com>
2022-07-27 12:31:49 -0400

    sandbox end

--- sandbox/client/src/components/App.js
+++ sandbox/client/src/components/App.js
@@ -1,26 +1,30 @@
-import { Component } from 'react'
+import React from 'react'
+
+import Card from './shared/Card'
 
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      
-    }
-  }
+// render -> creat a node insert it into the DOM
+// mounting -> the first time a component is rednered to the dom
 
-
-
-  render() {
-
-    console.log(this.state);
-    
-    return (
-      <div></div>
-    );
-  }
+// routing logic global configurations 
+function App() {
+  return (
+    <>
+      <navbar></navbar>
+      <Card
+        background='lightCoral' 
+        text='more specific error card'/>
+      <Card text='normal card'/>
+      <Card text='third card'/>
+      <Card text='broken card'/>
+      <Card
+        background='lightBlue'
+        color='white' 
+        text='blue card'/>
+      <Card text='pokemon card'/>
+    </>
+  )
 }
-
 
 export default App;
 
--- sandbox/client/src/components/shared/Card.js
+++ sandbox/client/src/components/shared/Card.js
@@ -1,9 +1,25 @@
-import Button from './Button'
+import React from 'react'
 
-function Card() {
+
+// function component 
+function Card(props) {
+     // console.log(propsback)
+
+     const exampleElement = <h4>This is {props.text}</h4>
+
      return (
-          <div>
-          </div>
+        <div style={{
+          color: props.color,
+          background: props.background,
+          height: '80px',
+          width: '200px',
+          padding: '4px',
+          margin: '10px',
+          cursor: 'pointer',
+          border: '2px solid black' 
+        }}>
+          {exampleElement}
+        </div>  
      )
 }
 
