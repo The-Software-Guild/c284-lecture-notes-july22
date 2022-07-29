614d85925266e782057d56586d22c4a12dce2e87
Patrick <pjmorgan716@gmail.com>
2022-07-29 09:39:52 -0400

    exercise hot fix

---
+++ exercises/routing-pages/package/src/components/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavigationBar from "./NavigationBar";
+import HomePage from "./pages/Homepage";
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
